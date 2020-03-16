eeede582fa3b7c4911720533ca6bd3a728b49d92
知行合一 <tom_9163@outlook.com>
2020-03-16 18:51:43 +0800

    Using _Bool.

--- Work3/Problem1.c
+++ Work3/Problem1.c
@@ -5,22 +5,35 @@
 
 #define INPUT_ERROR 1
 #define DOUBLE_MIN 1e-15
+#define FLOAT_MIN 1e-7
 
-int isDoubleEqualsZero(double num);
+/*
+    评测数据分析：
+    测试点0 - 两个不等实根
+    测试点1 - 
+    测试点2 - a=0,输出一个根
+    测试点3 - Zero Equation
+    测试点4 - Not an Equation
+    测试点5 - 两个相等实根
+    测试点6 - 
+*/
+
+_Bool isFloatEqualsZero(float num);
 
 int main()
 {
     float a, b, c;
-    if(scanf("%f %f %f", &a, &b, &c) < 0) {
+    if (scanf("%f %f %f", &a, &b, &c) < 0)
+    {
         perror("Input Err.");
         return INPUT_ERROR;
     }
 
-    if (isDoubleEqualsZero(a))
+    if (isFloatEqualsZero(a))
     {
-        if (isDoubleEqualsZero(b))
+        if (isFloatEqualsZero(b))
         {
-            if (isDoubleEqualsZero(c))
+            if (isFloatEqualsZero(c))
             {
                 // 都为0
                 printf("Zero Equation\n");
@@ -40,7 +53,7 @@ int main()
     double delta = pow(b, 2) - 4 * a * c;
 
     // 两个相等实根
-    if (isDoubleEqualsZero(delta))
+    if (isFloatEqualsZero(delta))
     {
         goto oneSolution;
     }
@@ -62,10 +75,10 @@ int main()
     if (delta < 0)
     {
         float complex result;
-        result = (-b+I * sqrt(-delta))/(2*a);
+        result = (-b + I * sqrt(-delta)) / (2 * a);
         float real = crealf(result);
         float imag_abs = fabs(cimagf(result));
-        
+
         printf("%.2f+%.2fi\n", real, imag_abs);
         printf("%.2f-%.2fi\n", real, imag_abs);
     }
@@ -73,7 +86,7 @@ int main()
     return 0;
 }
 
-int isDoubleEqualsZero(double num)
+_Bool isFloatEqualsZero(float num)
 {
     return fabs(num) <= DOUBLE_MIN ? 1 : 0;
 }
