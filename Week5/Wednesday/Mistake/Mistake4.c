3a5a1a52f09f5bfde09637dd20724babc9a2fffd
知行合一 <tom_9163@outlook.com>
2020-03-25 16:52:39 +0800

    Mistake4 finished.

--- Week5/Wednesday/Mistake/Mistake4.c
+++ Week5/Wednesday/Mistake/Mistake4.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <math.h>
+#define DOUBLE_MIN 1e-15
+
 int main()
 {
     double a, b;
     char op;
     scanf("%lf%c%lf", &a, &op, &b);
-    if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%')
+    if (op == '+' || op == '-' || op == '*' || op == '/')
     {
         if (op == '/')
         {
-            if (b == 0)
+            if (fabs(b) == 0)
             {
                 printf("Divisor can not be 0!");
             }
@@ -30,17 +32,20 @@ int main()
         {
             printf("%.2f", a * b);
         }
-        else if (op == '%')
-        {
-            if (a % b == 0)
-            {
-                printf("Unknown operator!");
-            }
-            else
-            {
-                printf("%d", a % b);
-            }
-        }
+        // else if (op == '%')
+        // {
+
+        //     if ((int)a % (int)b == 0)
+        //     {
+        //         printf("Unknown operator!");
+        //     }
+        //     else
+        //     {
+        //         printf("%d", (int)a % (int)b);
+        //     }
+        // }
+        return 0;
     }
+    puts("Unknown operator!");
     return 0;
 }
