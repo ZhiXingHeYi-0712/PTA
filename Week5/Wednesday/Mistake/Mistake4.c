3ed4910571ba01159e64ea98c65afcc293e1a239
知行合一 <tom_9163@outlook.com>
2020-03-25 16:48:29 +0800

    Add Mistake 4 (origin)

---
+++ Week5/Wednesday/Mistake/Mistake4.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    double a, b;
+    char op;
+    scanf("%lf%c%lf", &a, &op, &b);
+    if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%')
+    {
+        if (op == '/')
+        {
+            if (b == 0)
+            {
+                printf("Divisor can not be 0!");
+            }
+            else
+            {
+                printf("%.2f", a / b);
+            }
+        }
+        else if (op == '+')
+        {
+            printf("%.2f", a + b);
+        }
+        else if (op == '-')
+        {
+            printf("%.2f", a - b);
+        }
+        else if (op == '*')
+        {
+            printf("%.2f", a * b);
+        }
+        else if (op == '%')
+        {
+            if (a % b == 0)
+            {
+                printf("Unknown operator!");
+            }
+            else
+            {
+                printf("%d", a % b);
+            }
+        }
+    }
+    return 0;
+}
